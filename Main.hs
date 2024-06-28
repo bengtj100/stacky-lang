dddeacfe6d4f67b80d13f19406795b21766ef69b
Bengt Johansson <bengtj100@gmail.com>
2024-06-28 22:30:32 +0200

    Added command line arguments to the interpreter

--- Main.hs
+++ Main.hs
@@ -9,12 +9,16 @@
 --
 -- ====================================================================================================
 
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
 import System.IO.Error
 import System.Exit
 import System.Environment
 import Control.Exception
+
+import Text.RawString.QQ
 
 import CoreTypes
 import Repl
@@ -55,10 +59,31 @@ printVersion =
 
 printUsage :: IO ()
 printUsage =
-    do putStrLn "Usage ..."
-       putStrLn ""
+    do pName <- getProgName
+       putStrLn $ usageStr pName
        exitWith ExitSuccess
 
+usageStr :: String -> String
+usageStr pName =
+    [r|
+Usage: |] ++ pName ++ [r| ( <option> | <module-name> )*
+
+Where option is one of:
+
+--eval, -e <stacky-code>    Evaluate <stacky-code>
+--interactive, -i           Run in interactive mode, i.e., run the REPL after Prelude and
+                            all modules are loaded.
+--batch, -b                 Run in batch mode, i.e., terminate once all modules are loaded.
+--version                   Print the current version and terminate.
+--help, -h                  Print this message and then terminate.
+
+The interpreter will load all modules and execute the '--eval' options in the
+order they appear on the command line. If in interactive mode, the REPL will
+run after all loading is complete and in batch mode, the interpreter will
+terminate once all is loaded and executed.
+
+|]
+    
 -- ====================================================================================================
 
 runRepl :: Cxt -> IO ()
